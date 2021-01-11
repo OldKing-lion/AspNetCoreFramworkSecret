4b57a3728b94001b9944f526202427367d291cc5
lionkon <342501033@qq.com>
2021-01-11 17:43:30 +0800

    第一章学习记录，从Console模板演变到Mvc

---
+++ L1Demo/helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder
+                    .UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+}
